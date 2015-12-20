0ba5e965c9532ffa2bdc4efccc1b7df83de7a3ea
vpramo <pramod.venkatesh@ril.com>
2015-12-20 23:24:16 +0530

    Added vrouter specific configs

---
+++ manifests/contrail/vrouter_standalone.pp
@@ -0,0 +1,23 @@
+###
+# Class: rjil::contrail::vrouter_standalone
+#
+###
+
+class rjil::contrail::vrouter_standalone (
+  $discovery_address = join(service_discover_dns('lb.neutron.service.consul','name')),
+  $api_address       = undef,
+  $vrouter_physical_interface = 'eth1'
+) {
+
+  include rjil::jiocloud
+  include rjil::default_manifest
+
+
+  class {'::contrail::vrouter':
+    discovery_address => $discovery_address,
+    api_address       => $api_address,
+    vrouter_physical_interface => $vrouter_physical_interface,
+  }
+
+
+}
