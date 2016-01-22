722e0b06a11747d847493bd0627ff41af88b20ec
Saju Madhavan <sajuptpm@gmail.com>
2016-01-22 12:01:28 +0530

    make service name unique
    * show xmpp peer down notification only for 5 minute
    * changes status error to critical

--- files/monitor_scripts/contrail_control_introspect.sh
+++ files/monitor_scripts/contrail_control_introspect.sh
@@ -5,20 +5,31 @@ import xml.etree.ElementTree as ET
 from datetime import datetime
 from datetime import timedelta
 
+
 HOST="127.0.0.1"
 PROTOCOL="http"
 PORT=8083
 
 STATUS = {"success" : 0,
     "warning" : 1,
-    "error" : 2,
+    "critical" : 2,
      }
 
 OUTPUT_FORMAT = "{status} {entity} - {message}"
 
+
 def get_time(micsecs):
     utc_time = datetime(1970, 1, 1) + timedelta(milliseconds=int(micsecs)/1000)
     return utc_time
+
+
+def can_notify(utc_time, notify_minutes=10):
+    t = datetime.now() - utc_time
+    minutes = t.seconds/60
+    if minutes < notify_minutes:
+       return True
+    return False
+
 
 class Base(object):
     @classmethod
@@ -41,6 +52,7 @@ class Base(object):
         res = requests.get("{protocol}://{host}:{port}/{action}".\
                     format(protocol=PROTOCOL, host=HOST, port=PORT,action=action))
         return res
+
 
 class XmppServer(Base):
     @classmethod
@@ -106,11 +118,14 @@ class XmppServer(Base):
                     last_event_text = e_last_event.text
                     last_event_at_text = e_last_event_at.text
                     if e_deleted is not None:
-                        status = STATUS.get("error")
-                        msg = "WARNING: " + "Peer:'{name}', State:'{state}, {send_state}', LastEvent:{last_event}, LastEventAt={last_event_at}".\
-                        format(name=peer_name, state=state_text, send_state=send_state_text, last_event=last_event_text, last_event_at=get_time(last_event_at_text))
-                        result.append(OUTPUT_FORMAT.format(status=status,
-                            entity="XMPP:Peer", message=msg))
+                        if can_notify(get_time(last_event_at_text)):
+                            status = STATUS.get("critical")
+                            msg = "CRITICAL: " + "State:'{state}, {send_state}', LastEvent:{last_event}, LastEventAt={last_event_at}".\
+                            format(state=state_text,
+                                send_state=send_state_text, last_event=last_event_text,
+                                last_event_at=get_time(last_event_at_text))
+                            result.append(OUTPUT_FORMAT.format(status=status,
+                                entity="XMPP:Peer:{name}".format(name=peer_name), message=msg))
         return result
 
 
@@ -136,25 +151,27 @@ class BgpPeer(Base):
                 elif r.get("encoding") in ["XMPP"]:
                     connected_xmpp_peers.append(peer_name)
                 if r.get("state") not in attrs["state"]["exp_value"] or r.get("send_state") not in attrs["send_state"]["exp_value"]:
-                    status = STATUS.get("error")
-                    msg = "CRITICAL: " + "Peer:'{name}', State:'{state}, {send_state}', LastEvent:'{last_event}', LastStateAt:{last_state_at}'.".\
-                            format(name=peer_name, state=r.get("state",""), send_state=r.get("send_state",""), last_event=r.get("last_event",""),
+                    status = STATUS.get("critical")
+                    msg = "CRITICAL: " + "State:'{state}, {send_state}', LastEvent:'{last_event}', LastStateAt:{last_state_at}'.".\
+                            format(state=r.get("state",""), send_state=r.get("send_state",""), last_event=r.get("last_event",""),
                                 last_state_at=r.get("last_state_at",""))
                 else:
                     status = STATUS.get("success")
-                    msg = "OK: " + "Peer:'{name}', State:'{state}, {send_state}', LastEvent:'{last_event}', LastStateAt:{last_state_at}'.".\
-                            format(name=peer_name, state=r.get("state",""), send_state=r.get("send_state",""), last_event=r.get("last_event",""),
+                    msg = "OK: " + "State:'{state}, {send_state}', LastEvent:'{last_event}', LastStateAt:{last_state_at}'.".\
+                            format(state=r.get("state",""), send_state=r.get("send_state",""), last_event=r.get("last_event",""),
                                 last_state_at=r.get("last_state_at",""))
-                result.append(OUTPUT_FORMAT.format(status=status, entity="{encoding}:peer".format(encoding=r.get("encoding")), message=msg))
+                result.append(OUTPUT_FORMAT.format(status=status,
+                                                entity="{encoding}:peer:{name}".format(encoding=r.get("encoding"), name=peer_name), 
+                                                message=msg))
             if not connected_bgp_peers:
-                status = STATUS.get("error")
+                status = STATUS.get("critical")
                 msg = "CRITICAL: " + "Could not find BGP Peers"
             else:
                 status = STATUS.get("success")
                 msg = ",".join(connected_bgp_peers)
             result.append(OUTPUT_FORMAT.format(status=status, entity="BGP:Peers", message=msg))
             if not connected_xmpp_peers:
-                status = STATUS.get("error")
+                status = STATUS.get("critical")
                 msg = "CRITICAL: " + "Could not find XMPP Peers"
             else:
                 status = STATUS.get("success")
@@ -191,7 +208,7 @@ class Ifmap(Base):
                           monit_status = STATUS.get("success")
                        else:
                           monit_status_msg = "CRITICAL"
-                          monit_status = STATUS.get("error")
+                          monit_status = STATUS.get("critical")
                        msg = "{monit_status_msg}: Connection to Ifmap Server:{host} {conn_status}".\
                                format(monit_status_msg=monit_status_msg, host=host, conn_status=conn_status)
                        return OUTPUT_FORMAT.format(status=monit_status, entity="IFMAP:Connection", message=msg)
@@ -207,8 +224,8 @@ if __name__ == "__main__":
         try:
             res = fun()
         except Exception as ex:
-            print OUTPUT_FORMAT.format(status=STATUS.get("error"), entity="Monitoring Script", 
-                message="Failed:{script}, {reason}".format(script=fun.__name__, reason=str(ex)))
+            print OUTPUT_FORMAT.format(status=STATUS.get("critical"), entity="Monitoring Script:{script}".format(script=fun.__name__),
+                message="CRITICAL: {reason}".format(reason=str(ex)))
         if res:
             if isinstance(res, list):
                 for r in res:
