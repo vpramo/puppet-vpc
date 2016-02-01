172fd956192a60f267aeb53d141ed72581512277
vpramo <pramod.venkatesh@ril.com>
2016-02-01 10:54:24 +0530

    Added Haproxy endpoint for ec2api and add ssl configs

--- manifests/contrail/ec2api.pp
+++ manifests/contrail/ec2api.pp
@@ -4,7 +4,8 @@
 
 class rjil::contrail::ec2api (
   $db_provision = true,
-  $keystone_user_create = false
+  $keystone_user_create = false,
+  $ec2api_public_port  = 8788,
 }{
 
   if $db_provision {
@@ -16,6 +17,17 @@ class rjil::contrail::ec2api (
 
   if $keystone_user_create {
     include ::ec2api::keystone::auth
+  }
+
+  rjil::test::check { 'ec2api':
+    type    => 'tcp',
+    address => '127.0.0.1',
+    port    => $ec2api_public_port,
+  }
+
+  rjil::jiocloud::consul::service { 'ec2api':
+    tags          => 'real',
+    port          => $ec2api_public_port,
   }
 
   include ::ec2api
