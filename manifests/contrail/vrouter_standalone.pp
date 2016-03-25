402a1ea68121cf5fa11dc4e89f9a567f29e2461e
root <vpramo@gmail.com>
2016-03-25 13:20:01 +0530

    corrected backup in

--- manifests/contrail/vrouter_standalone.pp
+++ manifests/contrail/vrouter_standalone.pp
@@ -7,7 +7,7 @@ class rjil::contrail::vrouter_standalone (
   $discovery_address = join(service_discover_dns('lb.neutron.service.consul','name')),
   $api_address       = undef,
   $vrouter_physical_interface = 'eth1',
-  $vrouter_physical_interface = undef,
+  $vrouter_physical_interface_backup = undef,
 ) {
 
   #include rjil::jiocloud
